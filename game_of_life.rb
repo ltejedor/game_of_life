eb906c3cb70cc7903805db005f2d8862e42160d7
Leandra Tejedor <lmtejedor@gmail.com>
2013-12-14 20:01:54 -0500

    Implement all four rules to take place each turn

--- game_of_life.rb
+++ game_of_life.rb
@@ -10,21 +10,109 @@ class Game
 		end
 	end
 
+	def tick!
+		next_round_live_cells = []
+		next_round_dead_cells = []
+
+		world.cells.each do |cell|
+			#Rule #1
+			if cell.alive? and world.live_neighbors_around_cell(cell).count < 2
+				next_round_dead_cells << cell
+			end
+			#Rule #2
+			if cell.alive? and ([2, 3].include? world.live_neighbors_around_cell(cell).count)
+				next_round_live_cells << cell
+			end
+			#Rule #3
+			if cell.alive? and world.live_neighbors_around_cell(cell).count > 3
+				next_round_dead_cells << cell
+			end
+			#Rule #4
+			if cell.dead? and world.live_neighbors_around_cell(cell).count == 3
+				next_round_live_cells << cell
+			end
+		end
+
+		next_round_live_cells.each do |cell|
+			cell.live!
+		end
+
+		next_round_dead_cells.each do |cell|
+			cell.die!
+		end
+	end
+
 end
 
 class World
-	attr_accessor :rows, :cols, :cell_grid
+	attr_accessor :rows, :cols, :cell_grid, :cells
 
 	def initialize(rows=3, cols=3)
 		@rows = rows
 		@cols = cols
+		@cells = []
 
 		@cell_grid = Array.new(rows) do |row|
 									Array.new(cols) do |col|
-										Cell.new(col, row)
+										cell = Cell.new(col, row)
+										cells << cell
+										cell
 									end
 								end
 
+	end
+
+	def live_neighbors_around_cell(cell)
+		live_neighbors = []
+		# neighbor to the North
+		if cell.y > 0
+			candidate = self.cell_grid[cell.y - 1][cell.x]
+			live_neighbors << candidate if candidate.alive?
+		end
+
+		#neighbor to the north-west
+		if cell.y > 0 and cell.x > 0
+			candidate = self.cell_grid[cell.y - 1][cell.x - 1]
+			live_neighbors << candidate if candidate.alive?
+		end
+
+		#neighbor to the north-east
+		if cell.y > 0 and cell.x < (cols - 1)
+			candidate = self.cell_grid[cell.y - 1][cell.x + 1]
+			live_neighbors << candidate if candidate.alive?
+		end
+
+		#neighbor to the south-east
+		if cell.y < (rows - 1) and cell.x < (cols - 1)
+			candidate = self.cell_grid[cell.y + 1][cell.x + 1]
+			live_neighbors << candidate if candidate.alive?
+		end
+
+		#neighbor to the south-west
+		if cell.y < (rows - 1) and cell.x > 0
+			candidate = self.cell_grid[cell.y + 1][cell.x - 1]
+			live_neighbors << candidate if candidate.alive?
+		end
+
+		#neighbour to the east
+		if cell.x < (cols - 1)
+			candidate = self.cell_grid[cell.y][cell.x + 1]
+			live_neighbors << candidate if candidate.alive?
+		end
+
+		#neighbor to the south
+		if cell.y < (rows - 1)
+			candidate = self.cell_grid[cell.y + 1][cell.x]
+			live_neighbors << candidate if candidate.alive?
+		end
+
+		#neighbor to the west
+		if cell.x > 0
+			candidate = self.cell_grid[cell.y][cell.x - 1]
+			live_neighbors << candidate if candidate.alive?
+		end
+
+		live_neighbors
 	end
 
 end
@@ -38,7 +126,15 @@ class Cell
 		@y = y
 	end
 
-	def alive?
-		alive
+	def alive?; alive; end
+
+	def dead?; !alive; end
+
+	def die!
+		@alive = false
+	end
+
+	def live!
+		@alive = true
 	end
 end
--- rspec_game_of_life.rb
+++ rspec_game_of_life.rb
@@ -4,6 +4,7 @@ require_relative 'game_of_life.rb'
 describe 'Game of life' do
 
 	let!(:world) { World.new }
+	let!(:cell) { Cell.new(1, 1) }
 
 	context 'World' do
 		subject { World.new }
@@ -16,6 +17,8 @@ describe 'Game of life' do
 			subject.should respond_to(:rows)
 			subject.should respond_to(:cols)
 			subject.should respond_to(:cell_grid)
+			subject.should respond_to(:live_neighbors_around_cell)
+			subject.should respond_to(:cells)
 		end
 
 		it "should create proper cel grid on initialization" do
@@ -28,6 +31,45 @@ describe 'Game of life' do
 				end
 			end
 
+		end
+
+		it "should add all cells to cells array" do
+			subject.cells.count.should == 9
+		end
+
+		it "should detect a neighbor to the north" do
+			subject.cell_grid[0][1].should be_dead
+			subject.cell_grid[0][1].alive = true
+			subject.cell_grid[0][1].should be_alive
+			subject.live_neighbors_around_cell(subject.cell_grid[1][1]).count.should == 1
+		end
+		it "detects live neighbor to the north-east" do
+			subject.cell_grid[cell.y - 1][cell.x + 1].alive = true
+			subject.live_neighbors_around_cell(cell).count.should == 1
+		end
+		it "detects live neighbor to the east" do
+			subject.cell_grid[cell.y][cell.x + 1].alive = true
+			subject.live_neighbors_around_cell(cell).count.should == 1
+		end
+		it "detects live neighbour to the south-east" do
+			subject.cell_grid[cell.y + 1][cell.x + 1].alive = true
+			subject.live_neighbors_around_cell(cell).count.should == 1
+		end
+		it "detects live neighbor to the south" do
+			subject.cell_grid[cell.y + 1][cell.x].alive = true
+			subject.live_neighbors_around_cell(cell).count.should == 1
+		end
+		it "detects live neighbor to the south-west" do
+			subject.cell_grid[cell.y + 1][cell.x - 1].alive = true
+			subject.live_neighbors_around_cell(cell).count.should == 1
+		end
+		it "detects live neighbor to the west" do
+			subject.cell_grid[cell.y][cell.x - 1].alive = true
+			subject.live_neighbors_around_cell(cell).count.should == 1
+		end
+		it "detects live neighbor to the north-west" do
+			subject.cell_grid[cell.y - 1][cell.x - 1].alive = true
+			subject.live_neighbors_around_cell(cell).count.should == 1
 		end
 
 	end
@@ -45,6 +87,7 @@ describe 'Game of life' do
 			subject.should respond_to(:x)
 			subject.should respond_to(:y)
 			subject.should respond_to(:alive?)
+			subject.should respond_to(:die!)
 		end
 
 		it "should initialize properly" do
@@ -84,8 +127,61 @@ describe 'Game of life' do
 		let!(:game) { Game.new }
 
 		context "Rule #1: Any live cell with fewer than two live neighbours dies, as if caused by under-population." do
+
 			it "should kill a live cell with one live neighbor neighbor" do
+				game = Game.new(world, [[1, 0], [2, 0]])
+				game.tick!
+				world.cell_grid[1][0].should be_dead
+				world.cell_grid[2][0].should be_dead
 			end
+
+		end
+
+		context "Rule #2: Any live cell with two or three live neighbours lives on to the next generation." do
+
+			it "should not kill a cell with two live neighbors" do
+				game = Game.new(world, [[1, 0], [2, 0], [1, 1]])
+				game.tick!
+				world.cell_grid[1][0].should be_alive
+				world.cell_grid[2][0].should be_alive
+				world.cell_grid[1][1].should be_alive
+			end
+
+			it "should not kill a cell with three live neighbors" do
+				game = Game.new(world, [[1, 0], [2, 0], [2, 1], [1,1]])
+				game.tick!
+				world.cell_grid[1][0].should be_alive
+				world.cell_grid[2][0].should be_alive
+				world.cell_grid[1][1].should be_alive
+				world.cell_grid[2][1].should be_alive
+			end
+		end
+
+		context "Rule #3: Any live cell with more than three live neighbours dies, as if by overcrowding." do
+
+			it "should kill a cell with more than three live neighbors" do
+				game = Game.new(world, [[1, 0], [2, 0], [1, 1], [2, 1], [0, 0]])
+				game.tick!
+				world.cell_grid[0][0].should be_alive
+				world.cell_grid[1][0].should be_dead
+				world.cell_grid[2][0].should be_alive
+				world.cell_grid[1][1].should be_dead
+				world.cell_grid[2][1].should be_alive
+			end
+		end
+
+		context "Rule #4: Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction." do
+
+			it "should bring a cell back to life with exactly three live neighbors" do
+				game = Game.new(world, [[1, 0], [2, 0], [2, 1], [1,1]])
+				world.cell_grid[1][0].alive = false
+				world.cell_grid[1][0].should be_dead
+				game.tick!
+				world.cell_grid[1][0].should be_alive
+				world.cell_grid[2][0].should be_alive
+				world.cell_grid[2][1].should be_alive
+			end
+
 		end
 
 	end
